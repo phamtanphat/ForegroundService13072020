dd94f3d62537574a761437fe677f627eb6832783
phatdroid94 <phatdroid94@gmail.com>
2020-09-16 19:20:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/foregroundservice13702020/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.foregroundservice13702020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mBtnStart;
+    Button mBtnStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnStart = findViewById(R.id.buttonStartForeground);
+        mBtnStop = findViewById(R.id.buttonStopForeground);
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+            }
+        });
+
+    }
+}
